75d09b947fc236cdedf460ed994845aba24c8bde
Aman Kumar Maddeshiya <aman.k.maddeshiya@zenesys.com>
2023-07-20 17:04:40 +0530

    rework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app2';
+  posts = [
+    {
+      title : 'Neat Tree',
+      imageUrl: '../assets/car.jpg',
+      username: 'nature',
+      content:'Some quick example text to build on the card title and make up the bulk of the card content.'
+    },
+    {
+      title : 'cheat Tree',
+      imageUrl: '../assets/car.jpg',
+      username: 'cloud',
+      content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      title : 'bumboo Tree',
+      imageUrl: '../assets/car.jpg',
+      username: 'sea',
+      content:'It has survived not only five centuries, but also the leap into electronic typesetting.'
+    },
+  ];
 }
